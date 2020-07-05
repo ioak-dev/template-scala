b8af27097634422f04f4b8ab069e678c76d487d6
Arun Kumar Selvaraj <dev@ioak.org>
2020-07-05 22:12:02 +0530

    Base version

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "template-scala-two"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/Demo.scala
@@ -0,0 +1,10 @@
+object Demo {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+    run()
+  }
+
+  def run() = {
+    println("Inside run")
+  }
+}
